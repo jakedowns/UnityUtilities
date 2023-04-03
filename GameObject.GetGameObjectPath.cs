26efd70096fc5cfef2aa658b5374b6d2ab2d0b1b
Jake Downs <mail@jakedowns.com>
2023-04-03 14:09:21 -0700

    Create GameObject.GetGameObjectPath.cs

---
+++ GameObject.GetGameObjectPath.cs
@@ -0,0 +1,10 @@
+public static string GetGameObjectPath(GameObject obj)
+{
+    string path = "/" + obj.name;
+    while (obj.transform.parent != null)
+    {
+        obj = obj.transform.parent.gameObject;
+        path = "/" + obj.name + path;
+    }
+    return path;
+}
